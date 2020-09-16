20be7459e1f33069614a417fb7e323f12baf70c8
Ilze Kalnina <ilze@skate.lv>
2020-09-16 10:19:56 +0300

    Initial Commit

---
+++ WebGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebGithub
+//
+//  Created by Kisacka on 16/09/2020.
+//  Copyright © 2020 Kisacka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
